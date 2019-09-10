e62839dd13229c56e7bdf9f81dbb0e52ff135086
Aaron Nelson <Pokemon_Aaron@hotmail.com>
2019-09-09 23:14:28 -0600

    Auto button has proper cycling functionality

--- Scripts/SpotlightScripts.js
+++ Scripts/SpotlightScripts.js
@@ -1,4 +1,5 @@
 /* Lab 1 Spotlight Control Scripts */
+var autoIsOn = 0;
 
 document.getElementById("off").addEventListener("click", offClicked);
 function offClicked(){
@@ -8,11 +9,65 @@ function offClicked(){
 		x[0].classList.remove("running");
 	}
 	document.getElementById("off").className="running";
+	stopAuto();
 }
 
 
 document.getElementById("green").addEventListener("click", greenClicked);
 function greenClicked(){
+	runGreen();
+	stopAuto();
+}
+
+
+document.getElementById("yellow").addEventListener("click", yellowClicked);
+function yellowClicked(){
+	runYellow();
+	stopAuto();
+}
+
+
+document.getElementById("red").addEventListener("click", redClicked);
+function redClicked(){
+	runRed();
+	stopAuto();
+}
+
+
+document.getElementById("auto").addEventListener("click", autoClicked);
+function autoClicked(){
+	if(autoIsOn == 1){return;};	// For some reason if auto is clicked multiple times while it is already running, the timing gets extremely messed up.
+	let x = document.getElementsByClassName("running");
+	while (x[0]){
+		x[0].classList.remove("running");
+	}
+	document.getElementById("auto").className="autoOn";
+	autoIsOn = 1;
+	runCycle();
+}
+
+function runCycle(){
+	runGreen();
+	timey = setTimeout(function(){
+		runYellow();
+	}, 1000);	
+	timer = setTimeout(function(){
+		runRed();
+	}, 2000);
+	timeg = setTimeout(function(){
+		runCycle();
+	}, 3000);
+}
+
+function stopAuto(){
+	document.getElementById("auto").classList.remove("autoOn");
+	autoIsOn = 0;
+	clearTimeout(timey);
+	clearTimeout(timer);
+	clearTimeout(timeg);
+}
+
+function runGreen(){
 	let x = document.getElementsByClassName("running");
 	while (x[0]){
 		x[0].classList.remove("running");
@@ -20,9 +75,7 @@ function greenClicked(){
 	document.getElementById("green").className="running";
 }
 
-
-document.getElementById("yellow").addEventListener("click", yellowClicked);
-function yellowClicked(){
+function runYellow(){
 	let x = document.getElementsByClassName("running");
 	while (x[0]){
 		x[0].classList.remove("running");
@@ -30,22 +83,10 @@ function yellowClicked(){
 	document.getElementById("yellow").className="running";
 }
 
-
-document.getElementById("red").addEventListener("click", redClicked);
-function redClicked(){
+function runRed(){
 	let x = document.getElementsByClassName("running");
 	while (x[0]){
 		x[0].classList.remove("running");
 	}
 	document.getElementById("red").className="running";
-}
-
-
-document.getElementById("auto").addEventListener("click", autoClicked);
-function autoClicked(){
-	let x = document.getElementsByClassName("running");
-	while (x[0]){
-		x[0].classList.remove("running");
-	}
-	document.getElementById("auto").className="running";
 }
