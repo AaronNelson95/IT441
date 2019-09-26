31e98407d48ea45fcb73d3ef928cf85166801600
Aaron Nelson <Pokemon_Aaron@hotmail.com>
2019-09-25 21:58:22 -0600

    Working Lab 2 Script

--- Lab 2/Lab_2_Wifi_Stoplight/Lab_2_Wifi_Stoplight.ino
+++ Lab 2/Lab_2_Wifi_Stoplight/Lab_2_Wifi_Stoplight.ino
@@ -1,178 +1,140 @@
 /*
-    This sketch demonstrates how to set up a simple HTTP-like server.
-    The server will set a GPIO pin depending on the request
-      http://server_ip/gpio/0 will set the GPIO2 low,
-      http://server_ip/gpio/1 will set the GPIO2 high
-    server_ip is the IP address of the ESP8266 module, will be
-    printed to Serial when the module is connected.
-*/
-
+ * Aaron Nelson
+ * 9-24-19
+ */
 #include <ESP8266WiFi.h>
 
 #ifndef STASSID
 #define STASSID "YOUR SSID HERE"
-#define STAPSK  "YOUR WIFI PASS HERE"
+#define STAPSK  "YOUR PASSWORD HERE"
 #endif
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
+WiFiServer server(80);
+
 unsigned long beginningMillis;
 int autoModeNumber;
-const unsigned long period = 1000;
 
-// Create an instance of the server
-// specify the port to listen on as an argument
-WiFiServer server(80);
+int green = 12; // Pin D6
+int yellow = 13;  // Pin D7
+int red = 15; // Pin D8
+String currentMode = "off";
 
 void setup() {
   Serial.begin(115200);
-
+  pinMode(green, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(red, OUTPUT); 
+  autoModeNumber = 0;
+  lightsOff();
 
   // Connect to WiFi network
   Serial.println();
   Serial.println();
-  Serial.print(F("Connecting to "));
-  Serial.println(ssid);
+  Serial.print("Connecting to ");
+  Serial.print(ssid);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(F("."));
+    Serial.print(".");
   }
   Serial.println();
-  Serial.println(F("WiFi connected"));
+  Serial.println("WiFi connected");
 
   // Start the server
   server.begin();
-  Serial.println(F("Server started"));
-
-  // Print the IP address
+  Serial.println("Server started on ");
   Serial.println(WiFi.localIP());
 
-  // prepare LED
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(12, OUTPUT); // Pin D6
-  pinMode(13, OUTPUT); // Pin D7
-  pinMode(15, OUTPUT); // Pin D8
-  digitalWrite(LED_BUILTIN, 1);
-  digitalWrite(12, 0);
-  digitalWrite(13, 0);
-  digitalWrite(15, 0);
-  autoModeNumber = 0;
-  beginningMillis = millis();
 }
 
+
+
+
+
 void loop() {
-  // Check if a client has connected
+  // Check if a client is connected
   WiFiClient client = server.available();
-  if (!client) {
-    return;
-  }
-  //Serial.println(F("new client"));
-
-  client.setTimeout(5000); // default is 1000
-
-  // Read the first line of the request
-  String req = client.readStringUntil('\r');
-  //Serial.println(F("request: "));
-  //Serial.println(req);
-
-
-
-  // Match the request
-  String ModeOn = "off";
-  if (req.indexOf(F("/off")) != -1) {
-    ModeOn = "off";
-    autoModeNumber = 0;
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    digitalWrite(15, LOW);
-    Serial.println("Lights Off");
-  } else if (req.indexOf(F("/green")) != -1) {
-    ModeOn = "green";
-    // Green Light
-    autoModeNumber = 0;
-    digitalWrite(12, HIGH);
-    digitalWrite(13, LOW);
-    digitalWrite(15, LOW);
-    Serial.println("Green Light");
-  } else if (req.indexOf(F("/yellow")) != -1) {
-    ModeOn = "yellow";
-    // Yellow Light
-    autoModeNumber = 0;
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
-    digitalWrite(15, LOW);
-    Serial.println("Yellow Light");
-  } else if (req.indexOf(F("/red")) != -1) {
-    ModeOn = "red";
-    // Red Light
-    autoModeNumber = 0;
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    digitalWrite(15, HIGH);
-    Serial.println("Red Light");
-  } else if (req.indexOf(F("/auto")) != -1) {
-    ModeOn = "in auto mode";
-    if (autoModeNumber == 0){
-      beginningMillis = millis();
+  if (client) {
+    while (!client.available()) { // client hasn't sent info so wait for it
+      delay(10);
     }
-    autoModeNumber = 1;
+  }
+  client.setTimeout(100);
+
+  String req = client.readStringUntil('\r');
+  if (req.indexOf("/off") != -1) {
+    autoModeNumber = 0;
+    lightsOff();
+    currentMode = "off";
+  }
+  if (req.indexOf("/green") != -1) {
+    autoModeNumber = 0;
+    Serial.println("Green Light");
+    greenLight();
+    currentMode = "green";
+  }
+  if (req.indexOf("/yellow") != -1) {
+    autoModeNumber = 0;
+    Serial.println("Yellow Light");
+    yellowLight();
+    currentMode = "yellow";
+  }
+  if (req.indexOf("/red") != -1) {
+    autoModeNumber = 0;
+    Serial.println("Red Light");
+    redLight();
+    currentMode = "red";
+  }
+  if (req.indexOf("/auto") != -1) {
+    autoModeNumber = autoModeNumber + 1;
+    if (autoModeNumber == 4) {
+      autoModeNumber = 1;
+    }
     Serial.println(" - Auto Mode Activated - ");
-  } else {
-    Serial.println(F("invalid request"));
+    currentMode = "in auto mode";
+    beginningMillis = millis();
   }
 
 
-  // Check out auto mode information
-  if (autoModeNumber == 1){
-    // should be green
-    digitalWrite(12, HIGH);
-    digitalWrite(13, LOW);
-    digitalWrite(15, LOW);
-    Serial.println("Auto Green Light");
-  } else if (autoModeNumber == 2){
-    // should be yellow
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
-    digitalWrite(15, LOW);
-    Serial.println("Auto Yellow Light");
-  } else if (autoModeNumber == 3){
-    // should be red
-    digitalWrite(12, LOW);
-    digitalWrite(13, LOW);
-    digitalWrite(15, HIGH);
-    Serial.println("Auto Red Light");
+
+
+  if (autoModeNumber == 1) {
+    //Serial.println("Auto Green Light");
+    greenLight();
+  }
+  if (autoModeNumber == 2) {
+    //Serial.println("Auto Yellow Light");
+    yellowLight();
+  }
+  if (autoModeNumber == 3) {
+    //Serial.println("Auto Red Light");
+    redLight();
   }
 
-  if (autoModeNumber > 0){
-    if (millis() - beginningMillis > period){
+  if (autoModeNumber > 0) {
+    //Serial.println(beginningMillis);
+    //Serial.println(millis());
+    if ((2000 + beginningMillis) < millis()) {
       autoModeNumber = autoModeNumber + 1;
       if (autoModeNumber == 4) {
         autoModeNumber = 1;
       }
       beginningMillis = millis();
+      delay(5);
     }
   }
 
 
 
-  // read/ignore the rest of the request
-  // do not client.flush(): it is for output only, see below
-  while (client.available()) {
-    // byte by byte is not very efficient
-    client.read();
-  }
-
-
-  // Send the response to the client
-  // it is OK for multiple small client.print/write,
-  // because nagle algorithm will group them into one single packet
+  // Show the webpage on the browser
   client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nThe stoplight is now "));
-  client.print(ModeOn);
+  client.print(currentMode);
   client.print(F("<br><br>Click <a href='http://"));
   client.print(WiFi.localIP());
   client.print(F("/off'>here</a> to switch Off.<br> Click <a href='http://"));
@@ -185,8 +147,37 @@ void loop() {
   client.print(WiFi.localIP());
   client.print(F("/auto'>here</a> to switch the light to auto mode.</html>"));
 
-  // The client will actually be *flushed* then disconnected
-  // when the function returns and 'client' object is destroyed (out-of-scope)
-  // flush = ensure written data are received by the other side
-  //Serial.println(F("Disconnecting from client"));
+
+
+
+}
+
+
+
+
+
+
+void lightsOff() {
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
+  Serial.println("Lights Off");
+}
+
+void greenLight() {
+  digitalWrite(green, HIGH);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, LOW);
+}
+
+void yellowLight() {
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, HIGH);
+  digitalWrite(red, LOW);
+}
+
+void redLight() {
+  digitalWrite(green, LOW);
+  digitalWrite(yellow, LOW);
+  digitalWrite(red, HIGH);
 }
