aa512d9337cf30ac5c571947eb69422f5e2c4a41
Aaron Nelson <Pokemon_Aaron@hotmail.com>
2019-10-27 13:43:31 -0600

    Commented Lab 4

--- Lab 4/Lab_4_Echo_Sensor/Lab_4_Echo_Sensor.ino
+++ Lab 4/Lab_4_Echo_Sensor/Lab_4_Echo_Sensor.ino
@@ -1,14 +1,13 @@
 /*
  * Created by Aaron Nelson
- * Lab 3 Vehicle Sensor
- * 10-14-19
- * This script is used for the ultrasonic sensor (HC-SR04) to send readings frequently to a server. It works in collaboration with the stoplight, which reads from this server.
+ * Lab 4 Event Bus
+ * 10-26-19
+ * This script is used for the ultrasonic sensor (HC-SR04). It first listens to the MQTT server (whether or not the reed switch is connected) and it posts sensor reading groupings to the server (to be red by the stoplight)
+ * 
+ * The MQTT portion of this script was obtained from https://arduinodiy.wordpress.com/2017/11/24/mqtt-for-beginners/
  * 
  * Original Script for the sensor functionality was created by Rui Santos, provided at:
  *      https://randomnerdtutorials.com/complete-guide-for-ultrasonic-sensor-hc-sr04/ 
- * 
- * The basic server setup and framework was obtianed from the Arduino Examples-
- *      File -> Examples -> ESP8266WIFI -> WiFiManualWebServer
  *
     Ultrasonic Sensor Pins:
         VCC: +5VDC
@@ -18,43 +17,41 @@
  */
 
 #include <ESP8266WiFi.h>            // This contains the libraries that allows the board to connect to wifi
-#include <PubSubClient.h>
+#include <PubSubClient.h>           // This is used for communication with the MQTT Server
 
-const char* ssid = "YOUR WIFI";     // ***Specify the name of your wifi
-const char* password = "YOUR PASSWORD";     // ***Specify the password for that wifi
+const char* ssid = "YOUR WIFI HERE";       // Specify the name of your wifi
+const char* password = "YOUR PASSWORD HERE";      // Specify the password for your wifi
 
-const char* mqttServer = "192.168.137.1"; 
-const int mqttPort = 1883; 
+const char* mqttServer = "192.168.137.1"; // The location of your MQTT Server
+const int mqttPort = 1883;                // The port number your MQTT Server is running on (1883 is the default for Mosquito)  
 
-
-
-WiFiClient echoClient;
-PubSubClient client(echoClient);
+WiFiClient echoClient;              // Name for our MQTT connection client
+PubSubClient client(echoClient);    // Creates a partially initialized client instance
 
 int trigPin = 13;                   // The trigger is on pin D7. If using a different pin, specify that here
-int echoPin = 12;                   // The echo is on pin D6. If using a differnet pin, specify that here
+int echoPin = 12;                   // The echo is on pin D6. If using a different pin, specify that here
 long duration, cm, inches;          // This code can read distances in both cm and inches. They will both be printed to the Serial Monitor
-int startSensor = 0;
+int startSensor = 0;                // When this is a 1, it means the door magnet is connected and that the sensor should obtain distance readings
 
-char color = 'o';
-char lastColor = 'g';
+char color = 'o';                   // Contains the current color the stoplight should turn (to be posted to the MQTT server)
+char lastColor = 'g';               // Contains the last color the stoplight should be (to only update on changed states)
 
 void setup() {                      // This runs when the board is first turned on
   Serial.begin (115200);            // This allows serial information when connected to a computer (in here, this shows a constant stream of sensor readings)
   
   pinMode(trigPin, OUTPUT);         // Trigger acts as an output (it sends off a wave)
   pinMode(echoPin, INPUT);          // Echo acts as in input (it listens for the wave to come back)
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH);
+  pinMode(LED_BUILTIN, OUTPUT);     // The LED light will work as an output and show when the sensor is currently collecting data
+  digitalWrite(LED_BUILTIN, HIGH);  // Initially turn the built in LED light off
   
-  // Connect to WiFi network
+  /* Connect to WiFi */
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");   // This is shown on the serial if connected to a computer
   Serial.print(ssid);               // It displays the wifi it is trying to connect to
 
   WiFi.mode(WIFI_STA);              // It sets the wifis mode to "Station" (rather than "AP" which is the Access Point mode)
-  WiFi.hostname("EchoSensor");
+  WiFi.hostname("EchoSensor");      // Hostname to uniquely identify our device
   WiFi.begin(ssid, password);       // Attempts to connect to wifi using provided credentials
 
   while (WiFi.status() != WL_CONNECTED) {     // While the wifi is not connected yet:
@@ -64,18 +61,18 @@ void setup() {                      // This runs when the board is first turned 
   Serial.println();
   Serial.println("WiFi connected"); // When it does connect, show that there was success
 
-  connectMQTT();
+  connectMQTT();                    // Runs the function to set up our MQTT connection
   
   // Blink LED quickly to show that it is connected
-  digitalWrite(LED_BUILTIN, LOW); // light turns on
+  digitalWrite(LED_BUILTIN, LOW);   // light turns on
   delay(100);
-  digitalWrite(LED_BUILTIN, HIGH); // light turns off
+  digitalWrite(LED_BUILTIN, HIGH);  // light turns off
   delay(100);
-  digitalWrite(LED_BUILTIN, LOW); // light turns on
+  digitalWrite(LED_BUILTIN, LOW);   // light turns on
   delay(100);
-  digitalWrite(LED_BUILTIN, HIGH); // light turns off
+  digitalWrite(LED_BUILTIN, HIGH);  // light turns off
   
-  client.publish("device/echoSensor", "Activated");
+  client.publish("device/echoSensor", "Activated");  // Post in the device channel that this device is on
   
 }
 
@@ -83,18 +80,16 @@ void setup() {                      // This runs when the board is first turned 
 
 
  
-void loop() {                       // This constantly cycles through, reading value from the sensor and printing that on the hosted server
-  client.loop();
+void loop() {                       // This constantly cycles through, performing MQTT tasks and if the door switch is connected, finding sensor distances and if a range change occurs, posting this to the server
+  client.loop();                    // Runs through the MQTT functions to process incoming messages and send publish data
 
-  if (!client.connected()){
+  if (!client.connected()){         // Occasionally the client drops connection randomly and stops working
     Serial.println("MQTT was Unexpectedly Disconnected");
-    connectMQTT();
+    connectMQTT();                  // If the connection dropped, try to reconnect to our server
   }
 
-
-
-  if (startSensor == 1){
-    digitalWrite(LED_BUILTIN, LOW); // light turns on
+  if (startSensor == 1){            // If the door sensor is connected, we can begin to collect echo sensor readings
+    digitalWrite(LED_BUILTIN, LOW); // Turn on the LED light to show we are allowed to gather data
     
     // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
     // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
@@ -110,11 +105,11 @@ void loop() {                       // This constantly cycles through, reading v
     pinMode(echoPin, INPUT);
     duration = pulseIn(echoPin, HIGH);
    
-    // Convert the time into a distance (becuase the sound is bounced, the total duration is divided by two to gain the distance
+    // Convert the time into a distance (because the sound is bounced, the total duration is divided by two to gain the distance
     cm = (duration/2) / 29.1;     // Divide by 29.1 or multiply by 0.0343 to obtain cm from ms
     inches = (duration/2) / 74;   // Divide by 74 or multiply by 0.0135 to obtain in from ms
   
-    // Show the reading of inches and centemeters in the Serial Monitor.
+    // Show the reading of inches and centimeters in the Serial Monitor.
     Serial.print(inches);
     Serial.print("in, ");
     Serial.print(cm);
@@ -122,8 +117,7 @@ void loop() {                       // This constantly cycles through, reading v
     Serial.println();
 
 
-
-
+    // Respond to the readings from the echo sensor. Change the color variable to represent the range of the distance
     if (cm > 200) {
       // This is likely an odd reading, do nothing... (if a room is bigger, expand this number)
     } else if (cm > 35){
@@ -139,21 +133,13 @@ void loop() {                       // This constantly cycles through, reading v
       // Distance is less than 10ms, show that the driver is too close and there is no walkway and they should back up
       color = 'b';
     }
-
-
-
-    
-  
-  
-
-  } else {
-    digitalWrite(LED_BUILTIN, HIGH); // light turns off
-    color = 'o';
+  } else {                        // The echo sensor is not authorized to work (the magnet switch is disconnected)
+    digitalWrite(LED_BUILTIN, HIGH); // The LED light turns off
+    color = 'o';                  // The stoplight state should be off because the door is closed
   }
 
-
-
-  if (!(color == lastColor)) {
+  // If there is a state change (the color range now is different than the last), post the new value to the MQTT server
+  if (!(color == lastColor)) {    // The color is different
     if (color == 'g') {
       client.publish("garage/echoSwitch/color", "g");
     } else if (color == 'y') {
@@ -165,46 +151,52 @@ void loop() {                       // This constantly cycles through, reading v
     } else if (color == 'o') {
       client.publish("garage/echoSwitch/color", "o");
     }
-    Serial.println(color);
-    lastColor = color;
+    Serial.println(color);        // Show the new color value in the serial monitor
+    lastColor = color;            // Recognize the new color as the last color
   }
   
-  // Wait half of a second before taking another sensor reading (this number is variablle, smaller amounts will occur faster but you don't want to bog down the server)
+  // Wait a tenth of a second before taking another sensor reading (this number is variablle, smaller amounts will occur faster)
   delay(100);
 }
 
 
 
 
-void connectMQTT() {
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
-  while(!client.connected()) {
+
+void connectMQTT() {              // When first connecting to MQTT or when it becomes disconnected, run this script
+  client.setServer(mqttServer, mqttPort);     // Setup the server connection with information the user provided
+  client.setCallback(callback);   // Setup the callback to listen to changes made on the MQTT server
+  while(!client.connected()) {    // If the MQTT server is not connected or becomes disconnected, try again
     Serial.println("connecting to MQTT...");
-    if (client.connect("EchoSensor")) {
+    if (client.connect("EchoSensor")) {       // Try to connect with this name
       Serial.println("Connected to MQTT");
     } else {
-      Serial.print("failed... Re");
-      Serial.print(client.state());
-      delay(2000);
+      Serial.print("failed... Re");           // Repeatedly says "failed... Reconnecting" as this loop cycles through
+      // Serial.print(client.state());           // Show in the Serial Monitor what the current state of the server connection is
+      delay(2000);                // Wait two seconds before trying to connect to the server again
     }
   } 
-  client.subscribe("garage/doorSwitch");
+  client.subscribe("garage/doorSwitch");      // Listen to this path on the server and when a change is noticed, run the callback function
 }
+
+
+
+
 
 void callback(char* topic, byte* payload, unsigned int length)
 {
-
+  // The callback listens to the MQTT server and checks for changes in the "garage/doorSwitch"
+  // This function occurs after a message was received
   Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
+  Serial.println(topic);          // Notify which subscribed path received a change
   Serial.print("Message:");
 
-  String value = "";
+  String value = "";              // To contain the value from the MQTT path
   for(int i = 0; i < length; i++) {
-    char ch = ((char)payload[i]);
-    value.concat(ch);
+    char ch = ((char)payload[i]); // Only one character is read at a time
+    value.concat(ch);             // So create "value" based on the string of these characters
   }
-  Serial.print(value);
-  startSensor = (value.toInt());
+  Serial.print(value);            // Show the value that came through in the serial monitor
+  startSensor = (value.toInt());  // If a one came through, then the loop() function can begin collecting sensor data. If a 0 came through, it will stop
   Serial.println();
 }
--- Lab 4/Lab_4_Stoplight/Lab_4_Stoplight.ino
+++ Lab 4/Lab_4_Stoplight/Lab_4_Stoplight.ino
@@ -1,11 +1,10 @@
 /*
  * Created by Aaron Nelson
- * Lab 3 Vehicle Sensor
- * 10-14-19
- * This script is used with the stoplight module to respond to and act as a client when reading data from a server that posts values from an ultrasonic sensor.
+ * Lab 4 Event Bus
+ * 10-26-19
+ * This script is used with the stoplight module. It will light up different colors based on what is passed to the MQTT server (posted from an ultrasonic sensor).
  *  
- * The basic client setup and framework was obtianed from the Arduino Examples-
- *      File -> Examples -> ESP8266WIFI -> WiFiClient
+ * The MQTT portion of this script was obtained from https://arduinodiy.wordpress.com/2017/11/24/mqtt-for-beginners/
  *
     Stoplight Pins:
         Green: Pin12 (D6)
@@ -15,19 +14,19 @@
 */
 
 #include <ESP8266WiFi.h>            // This contains the libraries that allows the board to connect to wifi
-#include <PubSubClient.h>
+#include <PubSubClient.h>           // This is used for communication with the MQTT Server
 
-const char* ssid = "YOUR WIFI";     // ***Specify the name of your wifi
-const char* password = "YOUR PASSWORD";     // ***Specify the password for that wifi
+const char* ssid = "YOUR WIFI HERE";       // Specify the name of your wifi
+const char* password = "YOUR PASSWORD HERE";      // Specify the password for your wifi
 
-const char* mqttServer = "192.168.137.1"; 
-const int mqttPort = 1883; 
+const char* mqttServer = "192.168.137.1"; // The location of your MQTT Server
+const int mqttPort = 1883;                // The port number your MQTT Server is running on (1883 is the default for Mosquito) 
 
 unsigned long beginningMillis;      // A variable to hold our current time (used for auto mode to keep track of how much time has passed)
-int onBlink;
+int onBlink;                        // Set if the server reading sent 'b'. Will change the red light on and off
 
-WiFiClient stoplightClient;
-PubSubClient client(stoplightClient);
+WiFiClient stoplightClient;         // Name for our MQTT connection client
+PubSubClient client(stoplightClient);     // Creates a partially initialized client instance
 
 int green = 12;                     // Pin D6 is used for green. If using a different pin, specify that here
 int yellow = 13;                    // Pin D7 is used for yellow. If using a different pin, specify that here
@@ -36,21 +35,20 @@ int redFlash = 0;                   // While this value is 1, it specifies the l
 
 
 void setup() {                      // This runs when the board is first turned on
-  Serial.begin(115200);             // This allows serial information when connected to a computer (in here, this just tells what cm reading is seen from the server)
+  Serial.begin(115200);             // This allows serial information when connected to a computer (this will tell what echosensor message was sent to the MQTT server)
   pinMode(green, OUTPUT);           // Prepares the pin connected to the green light for output
   pinMode(yellow, OUTPUT);          // Prepares the pin connected to the yellow light for output
   pinMode(red, OUTPUT);             // Prepares the pin connected to the red light for output
-  lightsOff();
+  lightsOff();                      // Initialize the device by turning all of the stoplight lights off
 
-
-  // We start the client by connecting to a WiFi network
+  /* Connect to WiFi */
   Serial.println();
   Serial.println();
   Serial.print("Connecting to ");   // This is shown on the serial if connected to a computer
   Serial.println(ssid);             // It displays the wifi it is trying to connect to
 
   WiFi.mode(WIFI_STA);              // It sets the wifis mode to "Station" (rather than "AP" which is the Access Point mode)
-  WiFi.hostname("Stoplight");
+  WiFi.hostname("Stoplight");       // Hostname to uniquely identify our device
   WiFi.begin(ssid, password);       // Attempts to connect to wifi using provided credentials
 
   while (WiFi.status() != WL_CONNECTED) {     // While the wifi is not connected yet:
@@ -60,101 +58,108 @@ void setup() {                      // This runs when the board is first turned 
   Serial.println("");
   Serial.println("WiFi connected"); // When it does connect, show that there was success
 
-  connectMQTT();
+  connectMQTT();                    // Runs the function to set up our MQTT connection
   
-  // Blink LED quickly to show that it is connected
-  digitalWrite(LED_BUILTIN, LOW); // light turns on
+  // Blink LED quickly to show that it is connected to wifi and the MQTT server
+  digitalWrite(LED_BUILTIN, LOW);   // light turns on
   delay(100);
-  digitalWrite(LED_BUILTIN, HIGH); // light turns off
+  digitalWrite(LED_BUILTIN, HIGH);  // light turns off
   delay(100);
-  digitalWrite(LED_BUILTIN, LOW); // light turns on
+  digitalWrite(LED_BUILTIN, LOW);   // light turns on
   delay(100);
-  digitalWrite(LED_BUILTIN, HIGH); // light turns off
+  digitalWrite(LED_BUILTIN, HIGH);  // light turns off
   
-  client.publish("device/stoplight", "Activated");
-  
+  client.publish("device/stoplight", "Activated");  // Post in the device channel that this device is on
 }
 
 
 
 
 
-void loop() {                       // This constantly cycles through, checking the server and reflecting the proper stoplight color
-  client.loop();
+void loop() {                       // This constantly cycles through, checking the MQTT server for a message, reconnecting if it becomes disconnected, and blinking the red light if that setting is on
+  client.loop();                    // Runs through the MQTT functions to process incoming messages and send publish data
 
-  if (!client.connected()){
+  if (!client.connected()){         // Occasionally the client drops connection randomly and stops working
     Serial.println("MQTT was Unexpectedly Disconnected");
-    connectMQTT();
+    connectMQTT();                  // If the connection dropped, try to reconnect to our server
   }  
 
-  if (onBlink == 1){
-    if ((325 + beginningMillis) < millis()) {
-    
-      if (redFlash == 0){
-          redLight();
-          redFlash = 1;
-      } else if (redFlash == 1) {
-          lightsOff();
-          redFlash = 0;
+  if (onBlink == 1){                // If the red light is supposed to be blinking
+    if ((325 + beginningMillis) < millis()) {       // 325ms after the light last blinked
+      if (redFlash == 0){           // If the red light was off,
+          redLight();               // turn the red light on
+          redFlash = 1;             // and change the variable to have it turn off next time
+      } else if (redFlash == 1) {   // If the red light was on,
+          lightsOff();              // turn the red light off
+          redFlash = 0;             // and change the variable to ahve it turn on next time
       }
-      beginningMillis = millis();
+      beginningMillis = millis();   // Restart the counter for when the loop will run again
       delay(5);
     }
-
-    
   }
-  
 }
 
 
-void connectMQTT() {
-  client.setServer(mqttServer, mqttPort);
-  client.setCallback(callback);
-  while(!client.connected()) {
+
+
+
+void connectMQTT() {              // When first connecting to MQTT or when it becomes disconnected, run this script
+  client.setServer(mqttServer, mqttPort);     // Setup the server connection with information the user provided
+  client.setCallback(callback);   // Setup the callback to listen to changes made on the MQTT server
+  while(!client.connected()) {    // If the MQTT server is not connected or becomes disconnected, try again
     Serial.println("connecting to MQTT...");
-    if (client.connect("Stoplight")) {
+    if (client.connect("Stoplight")) {        // Try to connect with this name
       Serial.println("Connected to MQTT");
     } else {
-      Serial.print("failed... Re");
-      Serial.print(client.state());
-      delay(2000);
+      Serial.print("failed... Re");           // Repeatedly says "failed... Reconnecting" as this loop cycles through
+      // Serial.print(client.state());           // Show in the Serial Monitor what the current state of the server connection is
+      delay(2000);                // Wait two seconds before trying to connect to the server again
     }
   } 
-  client.subscribe("garage/echoSwitch/color");
+  client.subscribe("garage/echoSwitch/color");  // Listen to this path on the server and when a change is noticed, run the callback function
 }
+
+
+
+
 
 void callback(char* topic, byte* payload, unsigned int length)
 {
-
+  // The callback listens to the MQTT server and checks for changes in the "garage/echoSwitch/color"
+  // This function occurs after a message was received
   Serial.print("Message arrived in topic: ");
-  Serial.println(topic);
+  Serial.println(topic);          // Notify which subscribed path received a change
   Serial.print("Message:");
 
-  String value = "";
+  String value = "";              // To contain the value from the MQTT path
   for(int i = 0; i < length; i++) {
-    char ch = ((char)payload[i]);
-    value.concat(ch);
+    char ch = ((char)payload[i]); // Only one character is read at a time
+    value.concat(ch);             // So create "value" based on the string of these characters
   }
-  Serial.print(value);
+  Serial.print(value);            // Show the value that came through in the serial monitor
   Serial.println();
 
-
-
-
+  // Respond to the color the Echo Sensor sent to tell which color the light should turn
   if (value == "g") {
+    // Echo sensor is over 35 cm
     greenLight();
     onBlink = 0;
   } else if (value == "y") {
+    // Echo sensor is between 20-35 cm
     yellowLight();
     onBlink = 0;
   } else if (value == "r") {
+    // Echo sensor is between 10-20 cm
     redLight();
     onBlink = 0;
   } else if (value == "b") {
+    // Echo sensor is between 0-10 cm
     lightsOff();
+    // Because this is too close, we wish to flash the red light (functionality for this is in loop())
     onBlink = 1;
     beginningMillis = millis();
   } else if (value == "o") {
+    // The door switch became disconnected and the echo sensor is no longer reading values
     lightsOff();
     onBlink = 0;
   }
@@ -163,7 +168,8 @@ void callback(char* topic, byte* payload, unsigned int length)
 
 
 
-/* These are the functions that are called after a sensor reading is read. 
+
+/* These are the functions that are called depending on the read MQTT value. 
     They only turn on the light they are supposed to according to the pin number specified at the beginning of the script */
 void lightsOff() {
   digitalWrite(green, LOW);
